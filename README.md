3538f1b1d026646fa902a8bc6353f9ef23ee8dfe
Gia Thanh Vuong <thanhvg@gmail.com>
2015-12-21 09:40:12 -0700

    first version 0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Winnum
+A gnome shell extension based on AppKey
+Add Winkey + number to activate the corresponding window on the window list extension
+
+# TODO
+- change the uuid, it's goofy
+- add hotkey to cycle back and forth in the order appears on the window list extension
---
+++ extension.js
@@ -0,0 +1,96 @@
+//Import the Main and Meta object
+const Main = imports.ui.main;
+const Meta = imports.gi.Meta;
+const Lang = imports.lang;
+
+// Import shell
+const Shell = imports.gi.Shell;
+
+// Import the convenience.js (Used for loading settings schemas)
+const Self = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Self.imports.convenience;
+const Settings = Convenience.getSettings();
+
+function WinNum() {
+  this.init();
+}
+
+WinNum.prototype = {
+  Name: 'WinNum',
+
+  init: function() {
+   // this.settings = Convenience.getSettings();
+  },
+
+  handler: function(num) {
+    // var num2 = 1;
+    // Main.notify('inside handler');
+    // var windowListLength = global.screen.get_active_workspace().list_windows().length;
+    // if (windowListLength > num)
+    //  global.screen.get_active_workspace().list_windows()[num].activate(0);
+
+    let metaWorkspace = global.screen.get_active_workspace();
+    let windows = metaWorkspace.list_windows().filter(function(w) {return w && !w.skip_taskbar;});
+    windows.sort(function(w1, w2) {
+      return w1.get_stable_sequence() - w2.get_stable_sequence();
+    });
+    if (num == -1) windows[windows.length-1].activate(0);
+    else if (windows.length > num) windows[num].activate(0);
+  },
+
+  _addKeybindings: function(name, num) {
+    // Main.notify('inside _addKeybindings '+ num);
+    var ModeType = Shell.hasOwnProperty('ActionMode') ? Shell.ActionMode : Shell.KeyBindingMode;
+    // Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, this.handler(num)); - Not working
+    // Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, function(num) {this.handler(num)})); - not working
+
+    Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, function() {this.handler(num-1)}));
+
+    // working !
+    // Main.wm.addKeybinding(name,
+    //                      Settings,
+    //                      Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW,
+    //                      Lang.bind(this,
+    //                                function() {
+    //                                  var windowListLength = global.screen.get_active_workspace().list_windows().length;
+    //                                  if (windowListLength > num)
+    //                                    global.screen.get_active_workspace().list_windows()[num].activate(0);       }
+    //                               )
+    //                      );
+
+    // Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, this.handler(num))); - Not working
+
+  },
+
+  _removeKeybindings: function(name) {
+        	Main.wm.removeKeybinding(name);
+	},
+
+  enable: function() {
+    for(var i=0; i<10; i++) {
+      this._addKeybindings('app-key'+i,i);
+    }
+  },
+
+  disable: function() {
+    for(var i=0; i<10; i++) {
+      this._removeKeybindings('app-key'+i);
+    }
+
+  },
+};
+
+let app;
+
+// create app keys app
+function init() {
+	app = new WinNum();
+}
+
+function enable() {
+	app.enable();
+}
+
+function disable() {
+    app.disable();
+}
