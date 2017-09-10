7c73c272207773cd0e38c07c8b2b21ad312a9fe5
Thanh Vuong <thanhvg@gmail.com>
2017-09-10 00:45:28 -0600

    support dual monitors, shift-win-num to activate window on the other monitor

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A gnome shell extension based on AppKey
 
 Add Winkey + number to activate the corresponding window on the window list extension
-
+shif + winkey + number to activate window on inactive monitor (dual monitors)
 ## TODO
 - change the uuid, it's goofy
 - add hotkey to cycle back and forth in the order appears on the window list extension
--- extension.js
+++ extension.js
@@ -23,60 +23,60 @@ WinNum.prototype = {
   },
 
   handler: function(num) {
-    // var num2 = 1;
-    // Main.notify('inside handler');
-    // var windowListLength = global.screen.get_active_workspace().list_windows().length;
-    // if (windowListLength > num)
-    //  global.screen.get_active_workspace().list_windows()[num].activate(0);
-
+    let wn;
     let metaWorkspace = global.screen.get_active_workspace();
-    let windows = metaWorkspace.list_windows().filter(function(w) {return w && !w.skip_taskbar;});
-    windows.sort(function(w1, w2) {
-      return w1.get_stable_sequence() - w2.get_stable_sequence();
-    });
-    if (num == -1) windows[windows.length-1].activate(0);
-    else if (windows.length > num) windows[num].activate(0);
+    let focus_window = global.display.focus_window;
+    let monitor = focus_window.get_monitor();
+    let windows = metaWorkspace.list_windows()
+          .filter(function(w) {return w && !w.skip_taskbar && w.get_monitor() == monitor;})
+          .sort(function(w1, w2) {
+            return w1.get_stable_sequence() - w2.get_stable_sequence();
+          });
+    if (num == -1) wn = windows[windows.length-1];
+      else wn = windows[num];
+    if (wn == focus_window) wn.minimize();
+      else wn.activate(0);
   },
 
-  _addKeybindings: function(name, num) {
+
+  shiftHandler: function(num) {
+    let wn;
+    let metaWorkspace = global.screen.get_active_workspace();
+    let focus_window = global.display.focus_window;
+    let monitor = focus_window.get_monitor();
+    let windows = metaWorkspace.list_windows()
+          .filter(function(w) {return w && !w.skip_taskbar && w.get_monitor() != monitor;})
+          .sort(function(w1, w2) {
+            return w1.get_stable_sequence() - w2.get_stable_sequence();
+          });
+    if (num == -1) wn = windows[windows.length-1];
+    else wn = windows[num];
+    if (wn == focus_window) wn.minimize();
+    else wn.activate(0);
+  },
+
+  _addKeybindings: function(name, num, handler) {
     // Main.notify('inside _addKeybindings '+ num);
     var ModeType = Shell.hasOwnProperty('ActionMode') ? Shell.ActionMode : Shell.KeyBindingMode;
-    // Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, this.handler(num)); - Not working
-    // Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, function(num) {this.handler(num)})); - not working
-
-    Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, function() {this.handler(num-1)}));
-
-    // working !
-    // Main.wm.addKeybinding(name,
-    //                      Settings,
-    //                      Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW,
-    //                      Lang.bind(this,
-    //                                function() {
-    //                                  var windowListLength = global.screen.get_active_workspace().list_windows().length;
-    //                                  if (windowListLength > num)
-    //                                    global.screen.get_active_workspace().list_windows()[num].activate(0);       }
-    //                               )
-    //                      );
-
-    // Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, this.handler(num))); - Not working
-
+    Main.wm.addKeybinding(name, Settings, Meta.KeyBindingFlags.NONE, ModeType.NORMAL | ModeType.OVERVIEW, Lang.bind(this, function() {handler(num-1);}));
   },
 
   _removeKeybindings: function(name) {
-        	Main.wm.removeKeybinding(name);
+   	Main.wm.removeKeybinding(name);
 	},
 
   enable: function() {
     for(var i=0; i<10; i++) {
-      this._addKeybindings('app-key'+i,i);
+      this._addKeybindings('app-key'+i, i, this.handler);
+      this._addKeybindings('app-shift-key'+i, i, this.shiftHandler);
     }
   },
 
   disable: function() {
     for(var i=0; i<10; i++) {
       this._removeKeybindings('app-key'+i);
+      this._removeKeybindings('app-shift-key'+i);
     }
-
   },
 };
 
